ab3640684731f4f4ef678cd1d2273f2afa04b585
TA <pepper.ppeatns@gmail.com>
2019-07-07 14:23:39 +0900

    created RootSection&SearchbarSection

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="RootSection">
+      <div className="adSection">input add here</div>
+      <header className="header">
+        
+      </header>
+      <div className="SearchBarSection">
+        <img src="/logo.svg" alt="" />
+        <div class="SearchBar"><i class="fas fa-search"></i><input className="SearchInput" placeholder="SEARCH" /></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
